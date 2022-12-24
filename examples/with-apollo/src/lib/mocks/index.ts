a625ffa0513d6fe9dc1b57a25942af14af1aa482
Frank Cooke <frankcooke79@gmail.com>
2022-12-24 13:53:10 -0500

    refactor: mock tooling initialization in example

--- examples/with-apollo/src/lib/mocks/index.ts
+++ examples/with-apollo/src/lib/mocks/index.ts
@@ -2,11 +2,8 @@ import { mockBuilder } from './builder';
 import './operations';
 import './models';
 
-const { models, createDevtools, createProvider, createMockLink } = mockBuilder;
+export const { models } = mockBuilder;
 
-export const MockProvider = createProvider();
-export const Devtools = createDevtools();
-
-export const mockLink = createMockLink();
-
-export { models };
+export const MockProvider = mockBuilder.createProvider();
+export const Devtools = mockBuilder.createDevtools();
+export const mockLink = mockBuilder.createMockLink();
