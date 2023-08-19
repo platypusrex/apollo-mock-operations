36333ff4cbaca9813c2a28c30fd96eb54e4cdaca
Frank Cooke <frankcooke79@gmail.com>
2023-08-19 00:06:13 -0400

    chore: adds changeset

--- examples/with-vite/src/lib/mocks/builder.ts
+++ examples/with-vite/src/lib/mocks/builder.ts
@@ -1,7 +1,6 @@
 import { MockGQLOperations } from '@apollo-mock-operations/core';
 import introspectionResult from './introspection.json';
 
-console.log('boom', import.meta.env.VITE_MOCK_OPERATION);
 export const mockBuilder = new MockGQLOperations<
   MockOperations,
   OperationModels
